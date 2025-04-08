5d83951f2488683208b5293d51e392afa642e19b
CodyNewton <Cody.newton-1@colorado.edu>
2025-04-08 11:06:31 -0600

    Figure plotting for all of the sample values implemented.

--- Lab_3_main.m
+++ Lab_3_main.m
@@ -7,8 +7,8 @@ close all
 K_g = 33.3;
 K_m = 0.0401; % [Nm/amp]
 R_m = 19.2;   % [ohms]
-K_1=25;         % [K_p] btwn -2 and 50 Neg values mean unstable 
-K_3=0;         % [K_d] btwn -1.5 and 1.5 Neg values mean unstable 
+K_1=[10 20 5 10 10 10];         % [K_p] btwn -2 and 50 Neg values mean unstable 
+K_3=[0 0 0 1 -1 -0.5];         % [K_d] btwn -1.5 and 1.5 Neg values mean unstable 
 % Rigid Arm 
 J_hub = 0.0005;          % [kgm^2]
 J_extra = 0.2*(.2794^2); % [kgm^2]
@@ -28,8 +28,9 @@ K_arm = (2*pi*fc)^2*(J_L);
 
 
 %% Closed Loop System
-d0 = (K_1.*K_g.*K_m)./(J_total.*R_m);
-d1 = (((K_g.^2).*(K_m.^2)+K_3.*K_g.*K_m)./(J_total.*R_m));
+for n=1:6
+d0 = (K_1(n).*K_g.*K_m)./(J_total.*R_m);
+d1 = (((K_g.^2).*(K_m.^2)+K_3(n).*K_g.*K_m)./(J_total.*R_m));
 d2 = 1; 
 n1 = d0;
 num = n1;
@@ -38,9 +39,12 @@ sysTF = tf(num,den);
 %% Step Response
 t= 0:0.01:4;
 u=max(0,min(t-1,1));
-lsim(sysTF,u,t)
+%lsim(sysTF,u,t)
 [x,t] = step(sysTF);
 
 
-figure();
-plot(t,x.*0.5);
+figure(n);
+plot(t,x-0.5);
+figure(n+6);
+plot(t,x-1);
+end
